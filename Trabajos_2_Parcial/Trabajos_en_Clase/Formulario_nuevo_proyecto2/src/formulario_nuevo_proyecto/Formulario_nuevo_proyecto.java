d316f7e177c31e5ade21d35517dc10aa7a2170c9
RAUL-F-RAMOS <rframos1@uth.hn>
2026-03-24 20:53:49 -0600

    Repositorio II Parcial Raul Ramos

---
+++ Trabajos_2_Parcial/Trabajos_en_Clase/Formulario_nuevo_proyecto2/src/formulario_nuevo_proyecto/Formulario_nuevo_proyecto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formulario_nuevo_proyecto;
+
+/**
+ *
+ * @author raulr
+ */
+public class Formulario_nuevo_proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
